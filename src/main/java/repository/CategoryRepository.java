73d16408e77c316af427e79d0ad8d863f4b5ba2a
Quan-HN <quandan38.ht@gmail.com>
2025-03-20 20:15:48 +0700

    delete

--- src/main/java/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.AsmGD1.repository;
-
-import com.example.AsmGD1.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
--- src/main/java/repository/CheckoutRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.AsmGD1.repository;
-
-import com.example.AsmGD1.entity.Checkout;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CheckoutRepository extends JpaRepository<Checkout, Integer> {
-}
